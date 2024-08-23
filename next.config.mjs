2417dad0f4c1870893d689aa8e47e9bfb0db246d
jesuslobatoromero <jesuslobatoromero@gmail.com>
2024-08-23 12:52:25 +0200

    next.config.msj and fixes

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,24 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-images:{
-    remotePatterns:[
+    typescript: {
+      ignoreBuildErrors: true
+    },
+    images: {
+      remotePatterns: [
         {
-            protocol: 'https',
-            hostname: 'lovely-flamingo-139.convex.cloud'
-        }
-    ]
-}
-
-};
-
-export default nextConfig;
+          protocol: 'https',
+          hostname: 'lovely-flamingo-139.convex.cloud'
+        },
+        {
+          protocol: 'https',
+          hostname: 'sleek-capybara-771.convex.cloud'
+        },
+        {
+          protocol: 'https',
+          hostname: 'img.clerk.com'
+        },
+      ]
+    }
+  };
+  
+  export default nextConfig;
